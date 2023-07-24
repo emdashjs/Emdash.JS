7a2d031254d57021b9c8841c76f489876b062e00
Aaron Huggins <aaron.huggins@runbox.com>
2023-07-24 16:09:46 -0500

    Add is strong password validator

---
+++ backend/auth/isStrongPassword.ts
@@ -0,0 +1,158 @@
+/* Code taken in part from
+ * https://github.com/validatorjs/validator.js/blob/master/src/lib/isStrongPassword.js
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Chris O'Hara <cohara87@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+export type ScoringOptions = typeof defaultOptions;
+type PasswordAnalysis = {
+  length: number;
+  uniqueChars: number;
+  uppercaseCount: number;
+  lowercaseCount: number;
+  numberCount: number;
+  symbolCount: number;
+};
+
+const upperCaseRegex = /^[A-Z]$/;
+const lowerCaseRegex = /^[a-z]$/;
+const numberRegex = /^[0-9]$/;
+const symbolRegex = /^[-#!$@£%^&*()_+|~=`{}\[\]:";'<>?,.\/ ]$/;
+
+const defaultOptions = {
+  minLength: 8,
+  minLowercase: 1,
+  minUppercase: 1,
+  minNumbers: 1,
+  minSymbols: 1,
+  returnScore: false,
+  pointsPerUnique: 1,
+  pointsPerRepeat: 0.5,
+  pointsForContainingLower: 10,
+  pointsForContainingUpper: 10,
+  pointsForContainingNumber: 10,
+  pointsForContainingSymbol: 10,
+};
+
+/** Counts number of occurrences of each char in a string */
+function countChars(str: string) {
+  const result: Record<string, number> = {};
+  Array.from(str).forEach((char) => {
+    const curVal: number | undefined = result[char];
+    if (curVal) {
+      result[char] += 1;
+    } else {
+      result[char] = 1;
+    }
+  });
+  return result;
+}
+
+/** Return information about a password */
+function analyzePassword(password: string) {
+  const charMap = countChars(password);
+  const analysis: PasswordAnalysis = {
+    length: password.length,
+    uniqueChars: Object.keys(charMap).length,
+    uppercaseCount: 0,
+    lowercaseCount: 0,
+    numberCount: 0,
+    symbolCount: 0,
+  };
+  Object.keys(charMap).forEach((char) => {
+    if (upperCaseRegex.test(char)) {
+      analysis.uppercaseCount += charMap[char];
+    } else if (lowerCaseRegex.test(char)) {
+      analysis.lowercaseCount += charMap[char];
+    } else if (numberRegex.test(char)) {
+      analysis.numberCount += charMap[char];
+    } else if (symbolRegex.test(char)) {
+      analysis.symbolCount += charMap[char];
+    }
+  });
+  return analysis;
+}
+
+function scorePassword(
+  analysis: PasswordAnalysis,
+  scoringOptions: ScoringOptions,
+) {
+  let points = 0;
+  points += analysis.uniqueChars * scoringOptions.pointsPerUnique;
+  points += (analysis.length - analysis.uniqueChars) *
+    scoringOptions.pointsPerRepeat;
+  if (analysis.lowercaseCount > 0) {
+    points += scoringOptions.pointsForContainingLower;
+  }
+  if (analysis.uppercaseCount > 0) {
+    points += scoringOptions.pointsForContainingUpper;
+  }
+  if (analysis.numberCount > 0) {
+    points += scoringOptions.pointsForContainingNumber;
+  }
+  if (analysis.symbolCount > 0) {
+    points += scoringOptions.pointsForContainingSymbol;
+  }
+  return points;
+}
+
+export default function isStrongPassword(
+  str: string,
+  options?: Partial<ScoringOptions>,
+) {
+  const analysis = analyzePassword(str);
+  const merged = {
+    ...options,
+    ...defaultOptions,
+  };
+  if (merged.returnScore) {
+    return scorePassword(analysis, merged);
+  }
+  return analysis.length >= merged.minLength &&
+    analysis.lowercaseCount >= merged.minLowercase &&
+    analysis.uppercaseCount >= merged.minUppercase &&
+    analysis.numberCount >= merged.minNumbers &&
+    analysis.symbolCount >= merged.minSymbols;
+}
+
+export function getScoringOptions(optionString?: string) {
+  optionString = optionString ?? "";
+  const options: Partial<ScoringOptions> = {};
+  for (const pair of optionString.split(";")) {
+    const [keyRaw, valueRaw] = pair.trim().split(":");
+    const key = keyRaw.trim() as keyof ScoringOptions;
+    const value: string | undefined = valueRaw?.trim();
+    if (value && key in defaultOptions) {
+      if (key === "returnScore") {
+        options[key] = value === "true";
+      } else {
+        const num = Number.parseInt(value, 10);
+        if (!Number.isNaN(num)) {
+          options[key] = num;
+        }
+      }
+    }
+  }
+  return options;
+}
