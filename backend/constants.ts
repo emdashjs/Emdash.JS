0b03ad85aa5b9b9901d7f2d97993cf5775cbbd4e
Aaron Huggins <aaron.huggins@runbox.com>
2023-07-24 16:10:04 -0500

    Parse password rules at app start

--- backend/constants.ts
+++ backend/constants.ts
@@ -1,8 +1,16 @@
+import { getScoringOptions } from "./auth/isStrongPassword.ts";
+
+const defaultRules =
+  "minLength:12;minLowercase:2;minUppercase:2;minNumbers:2;minSymbols:2";
+const providedRules = Deno.env.get("APP_DATA_PASSWORD_RULES")?.trim() ?? "";
 export const APP_DATA = {
   FOLDER: Deno.env.get("APP_DATA_FOLDER") ?? "",
   NAME: Deno.env.get("APP_DATA_NAME") ?? "blogger.js",
   UUID: Deno.env.get("APP_DATA_UUID") ?? "bab51817-3eac-4726-8d3b-0a57f886e8bf",
   EMAIL: Deno.env.get("APP_DATA_EMAIL") ?? "",
+  PASSWORD_RULES: providedRules
+    ? getScoringOptions(providedRules)
+    : getScoringOptions(defaultRules),
 } as const;
 export const USER_ERROR = {
   NOT_AUTHENTICATED: "user cannot be authenticated.",
