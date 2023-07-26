6224a4b40e462a7fc677b63f14ed4a556f00085f
Aaron Huggins <aaron.huggins@runbox.com>
2023-07-25 21:26:02 -0500

    Move login method into own file

---
+++ frontend/api/login.ts
@@ -0,0 +1,26 @@
+import { Session, User } from "../../backend/auth/mod.ts";
+import { AUTH_ERROR } from "../../backend/constants.ts";
+import { RouteRender } from "../../backend/server/mod.ts";
+
+export const login = async function login(request, renderer) {
+  const body = await request.formData();
+  const email = body.get("email") as string;
+  const password = body.get("password") as string;
+  const user = await User.get(email);
+  try {
+    await user.authenticate(password);
+  } catch (error) {
+    const serverErrror = renderer.json({ error: `${error?.message}` }, {
+      status: error?.message === AUTH_ERROR.NOT_AUTHENTICATED ? 401 : 500,
+    });
+    return serverErrror;
+  }
+  const session = new Session(user);
+  const uuid = await session.getUuid();
+  const redirect = `${request.origin}${
+    request.url.searchParams.get("landing") ?? ""
+  }`;
+  const response = Response.redirect(redirect, 302);
+  response.headers.set("Cookie", `session=${uuid}; user=${user.id}`);
+  return response;
+} as RouteRender;
