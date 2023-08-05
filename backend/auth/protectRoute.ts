e2ba415c462ba3c626a3a14f05756d5e2c70722f
Aaron Huggins <aaron.huggins@runbox.com>
2023-08-04 22:12:34 -0500

    Support bearer tokens

--- backend/auth/protectRoute.ts
+++ backend/auth/protectRoute.ts
@@ -2,14 +2,19 @@ import { Renderer, RouteRender } from "../server/mod.ts";
 import { APP_DATA, ERROR, HTTP_CODE } from "../constants.ts";
 import { User } from "./User.ts";
 import { SessionToken } from "./SessionToken.ts";
+import { AccessToken } from "./AccessToken.ts";
 
 export function protectRoute(routeRender: RouteRender): RouteRender {
   return async (request) => {
     if (!(APP_DATA.FIRST_USER && await User.count() === 0)) {
       const clone = request.clone();
-      const auth = getUserAuth(clone.headers.get("Authorization"));
+      const bearerAuth = getBearerAuth(clone.headers.get("Authorization"));
+      const userAuth = getUserAuth(clone.headers.get("Authorization"));
       await request.session?.get();
-      const user = await User.get(auth.email ?? request.session?.userId ?? "@");
+      const user = await User.get(
+        bearerAuth?.record.userId ?? userAuth?.email ??
+          request.session?.userId ?? "@",
+      );
 
       if (user.internal.state === "enabled") {
         let authenticated = false;
@@ -20,22 +25,37 @@ export function protectRoute(routeRender: RouteRender): RouteRender {
           } catch (error) {
             // Skip NOT_AUTHENTICATED to try a password authentication.
             if (error?.message !== ERROR.AUTH.NOT_AUTHENTICATED) {
-              return unauthorizedResponse(clone.respondWith, error);
+              return unauthorizedResponse(clone.respondWith, { error });
             }
           }
         }
-        if (!authenticated && auth.email && auth.password) {
+        if (!authenticated && userAuth?.email && userAuth?.password) {
           try {
-            await user.authenticate(auth.password);
+            await user.authenticate(userAuth.password);
             request.session = new SessionToken();
             await request.session.createToken(user);
             authenticated = true;
           } catch (error) {
-            return unauthorizedResponse(clone.respondWith, error);
+            return unauthorizedResponse(clone.respondWith, { error });
+          }
+        }
+        if (!authenticated && bearerAuth) {
+          try {
+            await bearerAuth.record.authenticate(bearerAuth.token);
+            authenticated = true;
+          } catch (error) {
+            return unauthorizedResponse(clone.respondWith, {
+              error,
+              hostname: clone.url.hostname,
+              bearer: true,
+            });
           }
         }
         if (!authenticated) {
-          return unauthorizedResponse(clone.respondWith);
+          return unauthorizedResponse(clone.respondWith, {
+            hostname: clone.url.hostname,
+            bearer: bearerAuth !== undefined,
+          });
         }
       } else {
         return forbiddenResponse(clone.respondWith);
@@ -52,10 +72,18 @@ function forbiddenResponse(renderer: Renderer) {
   );
 }
 
-function getUserAuth(
-  raw?: string | null,
-): { email?: string; password?: string } {
-  if (raw) {
+function getBearerAuth(raw?: string | null) {
+  if (raw && raw.toLowerCase().startsWith("bearer ")) {
+    const token = raw.slice(6).trim();
+    return {
+      token,
+      record: AccessToken.fromToken(token),
+    };
+  }
+}
+
+function getUserAuth(raw?: string | null) {
+  if (raw && raw.toLowerCase().startsWith("basic ")) {
     const base64 = raw.slice(5).trim();
     const decoded = atob(base64);
     const [email, ...passwordParts] = decoded.split(":");
@@ -64,20 +92,33 @@ function getUserAuth(
       return { email, password };
     }
   }
-  return {};
 }
 
-// deno-lint-ignore no-explicit-any
-function unauthorizedResponse(renderer: Renderer, error?: any): Response {
+function unauthorizedResponse(
+  renderer: Renderer,
+  { error, hostname, bearer = false }: {
+    bearer?: boolean;
+    // deno-lint-ignore no-explicit-any
+    error?: any;
+    hostname?: string;
+  },
+): Response {
   if (!error || error?.message === ERROR.AUTH.NOT_AUTHENTICATED) {
     const response = renderer.json(
       { error: ERROR.AUTH.NOT_AUTHENTICATED },
       { status: HTTP_CODE.AUTH.NOT_AUTHENTICATED },
     );
-    response.headers.set(
-      "WWW-Authenticate",
-      'Basic realm="User Visible Realm", charset="UTF-8"',
-    );
+    if (bearer) {
+      response.headers.set(
+        "WWW-Authenticate",
+        hostname ? `Bearer realm="${hostname}"` : "Bearer",
+      );
+    } else {
+      response.headers.set(
+        "WWW-Authenticate",
+        'Basic realm="User Visible Realm", charset="UTF-8"',
+      );
+    }
     return response;
   }
   return renderer.json(
