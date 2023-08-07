3351690354ba56d293ae1f8a20b980fbbb057a68
Aaron Huggins <aaron.huggins@runbox.com>
2023-08-06 19:12:31 -0500

    Add purecss as dependency

--- frontend/ui/App.tsx
+++ frontend/ui/App.tsx
@@ -12,6 +12,11 @@ export function App(
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/png" href="/static/favicon.png"></link>
+        <link
+          rel="stylesheet"
+          href="/static/cdn.jsdelivr.net/npm/purecss/3.0.0/build/pure-min.css"
+          integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls"
+        />
       </Helmet>
       {children}
     </div>
