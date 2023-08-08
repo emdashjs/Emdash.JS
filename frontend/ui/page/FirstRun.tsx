58bfce1ea45275dd7f7093529c80387709a128e7
Aaron Huggins <aaron.huggins@runbox.com>
2023-08-07 19:11:30 -0500

    Add first run page to create user

---
+++ frontend/ui/page/FirstRun.tsx
@@ -0,0 +1,59 @@
+/** @jsx h */
+import { h } from "../../../deps.ts";
+
+export function FirstRun() {
+  return (
+    <section>
+      <h1>Create First User</h1>
+      <p>
+        This application install is running for the first time and has no users.
+        Please create your first user, which will have full admin access to this
+        installation.
+      </p>
+      <form
+        class="pure-form pure-form-stacked"
+        action={`/api/user`}
+        method="post"
+      >
+        <fieldset>
+          <div class="pure-control-group">
+            <label for="first_name">First name</label>
+            <input
+              required="true"
+              placeholder="Jean-Luc"
+              type="text"
+              name="first_name"
+            />
+          </div>
+          <div class="pure-control-group">
+            <label for="last_name">Last name</label>
+            <input
+              required="true"
+              placeholder="Picard"
+              type="text"
+              name="last_name"
+            />
+          </div>
+          <div class="pure-control-group">
+            <label for="email">Email</label>
+            <input
+              required="true"
+              placeholder="email@example.com"
+              type="text"
+              name="email"
+            />
+          </div>
+          <div class="pure-control-group">
+            <label for="password">Password</label>
+            <input required="true" name="password" type="password" />
+          </div>
+          <div class="pure-controls">
+            <button class="pure-button pure-button-primary" type="submit">
+              Create
+            </button>
+          </div>
+        </fieldset>
+      </form>
+    </section>
+  );
+}
--- frontend/ui/routes.tsx
+++ frontend/ui/routes.tsx
@@ -7,11 +7,21 @@ import { h } from "../../deps.ts";
 import { App } from "./App.tsx";
 import { Server } from "../../backend/server/Server.ts";
 import { Login } from "./page/Login.tsx";
+import { FirstRun } from "./page/FirstRun.tsx";
 
 export const uiRouter = Server.router();
 
 uiRouter.get("/", (context) => {
   context.state.render.html(<App>Hello, world!</App>);
+});
+
+uiRouter.get("/admin/first_run", async (context) => {
+  await context.state.authenticate();
+  context.state.render.html(() => (
+    <App>
+      <FirstRun />
+    </App>
+  ));
 });
 
 uiRouter.get("/login", (context) => {
